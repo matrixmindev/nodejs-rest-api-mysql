eaed35a763856b268bfa314685dfa35a73331950
miguelalejandrodev <163203781+miguelalejandrodev@users.noreply.github.com>
2025-03-08 03:04:35 -0500

    fix index.js log

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { app } from "./app.js"
 import { PORT } from "../config.js"
 
-console.log(PORT);
+
 
 app.listen(PORT, () => {
     console.log(`Server is runningg on port http://localhost:${PORT}`)
